3a1b4c31fe867b576a2bc969cd4a9022e572bb48
hprasetio <hprasetio@mangrovenusantara.org>
2017-07-28 11:18:53 +0800

    r script for cp

---
+++ prep/CP/cp_script.R
@@ -0,0 +1,14 @@
+# load packages
+library(dplyr)
+library(tidyr) # install.packages('tidyr')
+library(readr) # install.packages('readr')
+library(stringr) # install.packages('stringr')
+
+# set directory
+dir_cp <- '~/github/bali/prep/cp'
+setwd(dir_cp)
+
+dir_layers <- '~/github/bali/prep/Cp'
+
+# read in raw data file
+cp_raw <- read(file.path(dir_cp, 'abration.csv')
