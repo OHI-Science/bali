57314efb9bad219fa9226877fc06fcb74760c3a4
jules32 <jstewart@nceas.ucsb.edu>
2017-08-24 11:05:09 -0700

    update install_ohicore instructions for @dev

--- install_ohicore.R
+++ install_ohicore.R
@@ -1,10 +1,10 @@
 ## install_ohicore.r
 
-## install_ohicore.r installs the most current version of the `ohicore` R. package. 
-## You will need to only do this once. `ohicore` requires recent versions of R and RStudio. 
+## install_ohicore.r installs the most current version of the `ohicore` R. package.
+## You will need to only do this once. `ohicore` requires recent versions of R and RStudio.
   ## 1. Update R. Download the latest version at http://cran.r-project.org.
   ## 2. Update RStudio. Download the latest version at http://www.rstudio.com/products/rstudio/download
-  ## 3. Source or run this script. 
+  ## 3. Source or run this script.
 
 ## delete any existing version of `ohicore`
 for (p in c('ohicore')){
@@ -22,5 +22,5 @@ for (p in c('devtools', 'git2r')){
 }
 
 ## install most current version of ohicore from github. Warnings are fine, but make sure there are no errors.
-devtools::install_github('ohi-science/ohicore')
+devtools::install_github('ohi-science/ohicore@dev')
 
