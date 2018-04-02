801192ec6dea16e3ba5f4e4375ed79d30c05e5b7
twibawa <tejaarief@gmail.com>
2018-04-02 18:11:59 +0800

    Add files via upload

---
+++ prep/HAB/prepare_habitat_all.R
@@ -0,0 +1,48 @@
+#prepare coral_reef, mangrove, and seagrass data
+
+library(tidyverse)
+
+#prepare coral_reef, mangrove, and seagrass data
+
+setwd("~/github/bali")
+
+
+variables <- c("coral", "mangrove", "seagrass")
+
+
+	for (i in 1:length(variables)){
+		
+			#read each data variable	
+			if (variables[i] == "coral"){
+			
+				
+				dat <- readr::read_delim(file = paste("hab_", variables[i], "_reef.csv", sep = ""), delim = ";")
+				
+			}else{
+				
+				
+				dat <- readr::read_delim(file = paste("hab_", variables[i], ".csv", sep = ""), delim = ";")
+				
+			}
+		
+		
+		#order data based on region id and years
+		dat <- dat %>% rename(year = years) %>% arrange(rgn_id, year)
+		
+		#fill missing region with area_ha = NA
+		dat <- dat %>% complete(rgn_id = full_seq(x = c(rgn_id, 8), period = 1), year, fill = list(area_ha = NA))
+		
+		#add habitat name based on variable name
+		dat <- dat %>% mutate(habitat = variables[i]) %>% select(rgn_id, habitat, year, area_ha)
+		
+		## convert hectares to square kilometers
+		dat <- dat %>% mutate(km2 = area_ha * 0.01) %>% select(rgn_id, habitat, year, km2)
+
+		## save data layer
+		write_csv(dat, paste("prep/HAB/hab_", variables[i], "_complete.csv", sep = ""))
+		write_csv(dat, paste("region2017/layers/hab_", variables[i], "_complete.csv", sep = ""))
+		
+				
+	}
+
+
