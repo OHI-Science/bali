a19b6b8e18d71ad8a77755220c5d52149065b066
Teja <tejaarief@gmail.com>
2018-07-31 17:08:11 +0800

    prep MAR sustainable area

---
+++ prep/MAR/prepare_sustainable_mariculture_area.R
@@ -0,0 +1,57 @@
+#####################################################
+#prepare extent of sustainable mariculture area (km2)
+######################################################
+
+##############################################
+#dataset only available for Bali province, so
+#the script divides equally each annual data
+#into eight subregions
+##############################################
+
+rm(list = ls(all = T))
+
+library(tidyverse)
+library(dplyr)
+
+goal <- "mar"
+region <- "bali"
+year <- 2016
+
+filename <- "aquaculture_sustainable_area"
+
+
+directory <- paste("~/github/", region, "/", sep = "")
+dir_scripts <- paste(directory, "/prep/", toupper(goal) , "/", sep = "")
+setwd(dir_scripts)
+
+
+	#read data
+	##########		
+	dat <- readr::read_delim(file = paste(filename, ".csv", sep = ""), delim = ",")
+	
+
+	
+	#repeat each row into eight rows
+	################################
+	dat <- dat[rep(seq_len(nrow(dat)), each = 5),]
+	
+	#equally divided into eight sub-region and converted to km2
+	###########################################################
+	dat$km2 <- dat$sustainable_ha * 0.01
+	
+		
+	#add new variable that will be used as rgn_id
+	#############################################	
+	dat <- dat%>%group_by(rgn_id)%>%mutate(year = seq(2011,2015,1))%>%ungroup()
+	
+	dat <- dat%>%select(rgn_id, year, km2)
+	
+
+			## save data layer
+			##################
+	
+			write_csv(dat, paste(directory, "prep/", toupper(goal), "/", goal, "_", filename, "_", region, year, ".csv", sep = ""))
+			write_csv(dat, paste(directory, "region2017/layers/", goal, "_", filename, "_", region, year, ".csv", sep = ""))
+			
+		
+			
