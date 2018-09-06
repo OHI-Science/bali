3d4938a11a612b288aa5c70d3b6eaf923ac4ca09
Teja <tejaarief@gmail.com>
2018-09-06 18:32:39 +0800

    Add files via upload

--- region2017/calculate_scores.R
+++ region2017/calculate_scores.R
@@ -1,25 +1,32 @@
 ## calculate_scores.R
 
+
 ## This script calculates OHI scores with the `ohicore` package.
 ## - configure_toolbox.r ensures your files are properly configured for `ohicore`.
 ## - The `ohicore` function CalculateAll() calculates OHI scores.
 
 ## set working directory for all OHI calculations
-setwd("~/github/bali/region2017")
+setwd("~/github/bali/region2017/")
 
 ## run the configure_toolbox.r script to check configuration
-source("configure_toolbox.r")
+source("configure_toolbox.R")
 
 ## calculate scenario scores
 scores <- ohicore::CalculateAll(conf, layers)
 
+
+#determine date-hour when running the script
+#to be included in the output filename of scores
+datetime <- format(Sys.time(), "%d%b%Y_%X")
+
 ## save scores as scores.csv
-write.csv(scores, 'scores.csv', na='', row.names=FALSE)
+write.csv(scores, paste("scores_", datetime, ".csv", sep = ""), na='', row.names=FALSE)
 
-## visualizations ---
+# visualizations ---
 
-## source script (to be incorporated into ohicore)
-source('https://raw.githubusercontent.com/OHI-Science/arc/master/circle2016/plot_flower_local.R')
+ ## source script (to be incorporated into ohicore)
+ source('https://raw.githubusercontent.com/OHI-Science/arc/master/circle2016/plot_flower_local.R')
 
-PlotFlower(assessment_name = "Bali",
-           dir_fig_save    = "reports/figures")
+ PlotFlower(assessment_name = "Bali",
+            dir_fig_save    = "reports/figures")
+  
